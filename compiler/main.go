00b40b79f0cab5c1bc8fa36cc3bcaab305c490da
Jairo Gomez <98117329+JairoGH@users.noreply.github.com>
2025-06-04 18:58:15 -0600

    init project

---
+++ compiler/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	// Ajusta si tu módulo se llama diferente
+	"fmt"
+	/*
+		compiler "compiler/parser"
+
+		"github.com/antlr4-go/antlr/v4"
+
+	*/)
+
+func main() {
+	fmt.Println("Bienvenido!")
+}
